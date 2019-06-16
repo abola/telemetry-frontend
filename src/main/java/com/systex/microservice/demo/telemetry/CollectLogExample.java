9033a8c1d3acff5f6eeeb77bd92d971d3f7baa10
李書豪 <abolali@systex.com>
2019-06-16 14:02:11 +0800

    Initial commit

---
+++ src/main/java/com/systex/microservice/demo/telemetry/CollectLogExample.java
@@ -0,0 +1,53 @@
+package com.systex.microservice.demo.telemetry;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableMap;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+import java.util.UUID;
+
+
+@RestController
+@RequestMapping(path = "/telemetry")
+public class CollectLogExample {
+
+    Logger log = LogManager.getLogger(CollectLogExample.class);
+
+    /**
+     * 模擬購物車新增品項的 API 接口
+     * @param item
+     * @return
+     * @throws Exception
+     */
+    @GetMapping(path="/logging/demo/{item}/add")
+    public String item(@PathVariable("item") String item) throws Exception{
+
+        if (Strings.isNullOrEmpty(item) ) return "{\"status\":\"error\"}";
+        // 為本筆交易建立一組 ID
+        String uniqueId = UUID.randomUUID().toString();
+
+        // 將 ID 與 物品的代號加入 Log
+        Map<String, String> logMap = mapBuilder()
+                .put("uniqueId",uniqueId)
+                .put("item",item)
+                .build();
+        log.info(toJson(logMap));
+
+        return "{\"status\":\"done\"}";
+    }
+
+    private String toJson(Object obj) throws Exception{
+        return new ObjectMapper().writeValueAsString(obj);
+    }
+
+    private ImmutableMap.Builder mapBuilder() {
+        return ImmutableMap.<String, String>builder();
+    }
+}
---
+++ src/main/java/com/systex/microservice/demo/telemetry/TelemetryDemoApplication.java
@@ -0,0 +1,13 @@
+package com.systex.microservice.demo.telemetry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelemetryDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelemetryDemoApplication.class, args);
+	}
+
+}
