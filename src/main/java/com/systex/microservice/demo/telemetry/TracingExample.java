cef7d876cb57ecc9b44270545d13ba2d37428e55
李書豪 <abolali@systex.com>
2019-06-25 13:30:30 +0800

    增加 Tracing sample

---
+++ src/main/java/com/systex/microservice/demo/telemetry/TracingExample.java
@@ -0,0 +1,65 @@
+package com.systex.microservice.demo.telemetry;
+
+import com.google.common.collect.Maps;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@RestController
+@RequestMapping(path = "/tracing")
+public class TracingExample {
+
+    private @Autowired HttpServletRequest request;
+
+    Logger log = LogManager.getLogger(TracingExample.class);
+
+    /**
+     * 模擬購物車新增品項的 API 接口
+     * @param item
+     * @return
+     * @throws Exception
+     */
+    @GetMapping(path="/list")
+    public String item(@PathVariable("item") String item) throws Exception{
+
+        HttpResponse<String> response = Unirest.get("http://telemetry-backend:8080/get/listQuote")
+                .headers(getTraceHeaders()) // 將 tracing 相關 header 送給下一個  request
+                .header("cache-control", "no-cache")
+                .asString();
+
+        return response.getBody();
+    }
+
+    /**
+     * 回傳 Tracing 相關的 headers
+     * @return Map<String, String>
+     */
+    private Map<String, String> getTraceHeaders(){
+        Map<String, String> headers = Maps.newHashMap();
+
+        // 不存在的 header 不能給值
+        if ( request.getHeader("x-request-id") != null  )
+            headers.put("x-request-id", request.getHeader("x-request-id"));
+        if ( request.getHeader("x-b3-traceid") != null  )
+            headers.put("x-b3-traceid", request.getHeader("x-b3-traceid"));
+        if ( request.getHeader("x-b3-spanid") != null  )
+            headers.put("x-b3-spanid", request.getHeader("x-b3-spanid"));
+        if ( request.getHeader("x-b3-parentspanid") != null  )
+            headers.put("x-b3-parentspanid", request.getHeader("x-b3-parentspanid"));
+        if ( request.getHeader("x-b3-sampled") != null  )
+            headers.put("x-b3-sampled", request.getHeader("x-b3-sampled"));
+        if ( request.getHeader("x-b3-flags") != null  )
+            headers.put("x-b3-flags", request.getHeader("x-b3-flags"));
+
+        return headers;
+    }
+}
